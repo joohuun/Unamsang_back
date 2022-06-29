098ed0fe0c65528c9af05652f904af6c55cdf7c2
DESKTOP-REGETCI\SinYoung <uky00832@gmail.com>
2022-06-29 13:46:17 +0900

    feat: 로그인, 로그아웃, 회원 정보 조회 기능 구현

--- user/views.py
+++ user/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from django.contrib.auth import authenticate, login, logout
+
+from user.serializers import UserSerializer
 
 # Create your views here.
+class UserView(APIView):
+    
+    permission_classes = [permissions.AllowAny]
+
+    # 회원 정보 조회
+    def get(self, request):
+        return Response(UserSerializer(request.user).data)
+
+class UserAPIView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    # 로그인
+    def post(self, request):
+        username = request.data.get('username', '')
+        password = request.data.get('password', '')
+
+        user = authenticate(request, username=username, password=password)
+
+        if not user:
+            return Response({"error": "존재하지 않는 계정이거나 패스워드가 일치하지 않습니다."}, status=status.HTTP_401_UNAUTHORIZED)
+
+        login(request, user)
+        return Response({"message": "로그인 성공."}, status=status.HTTP_200_OK)
+
+    # 로그아웃
+    def delete(self, request):
+        logout(request)
+        return Response({"message": "로그아웃 성공!"}, status=status.HTTP_200_OK)
