af699049aa8b2cd26f699f45b52fe6287eaf8aea
DESKTOP-REGETCI\SinYoung <uky00832@gmail.com>
2022-06-29 10:33:22 +0900

    feat: user model 생성

--- user/models.py
+++ user/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from article.models import BaseModel
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, password=None):
+        if not username:
+            raise ValueError('Users must have an username')
+        user = self.model(
+            username=username,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, username, password=None):
+        user = self.create_user(
+            username=username,
+            password=password
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class User(BaseModel):
+    username = models.CharField('사용자 계정', max_length=50, unique =True)
+    password = models.CharField('비밀번호', max_length=128)
+    email = models.EmailField('이메일 주소', max_length=50)
+
+    USERNAME_FIELD = 'username'
+    
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return f"{self.username} "
+
+    def has_perm(self, perm, obj=None):
+        return True
+    
+    def has_module_perms(self, app_label): 
+        return True
+    
+    # admin 권한 설정
+    @property
+    def is_staff(self): 
+        return self.is_admin
