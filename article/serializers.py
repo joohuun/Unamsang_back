771579c52abfbb57fa9d5c783f4947c236a6607e
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-06-29 10:19:56 +0900

    feature comment model

---
+++ article/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Article as ArticleModel
+from .models import Comment as CommentModel
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()  # 유저시리얼라이저 안 씀
+
+    def get_user(self, obj):
+        return obj.user.username
+
+    class Meta:
+        model = CommentModel
+        fields = ['user', 'contents']
+
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, source="comment_set", read_only=True)
+    class Meta:
+        model = ArticleModel
+        fields = ['title', 'is_active', 'tags', 'exposure_end_date', 'image_location', 'image', 'comments']
