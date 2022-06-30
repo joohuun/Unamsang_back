24a12957510df70a87a9371ca4585e25b5915c46
DESKTOP-REGETCI\SinYoung <uky00832@gmail.com>
2022-06-30 15:12:46 +0900

    feat: 커스텀 jwt 사용과 기존 user 기능을 토큰 사용 방식으로 변경(로그인, 회원 정보 조회, 수정)

--- user/urls.py
+++ user/urls.py
@@ -3,18 +3,14 @@ from django.urls import path
 from user import views
 
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
+    # TokenObtainPairView,
     TokenRefreshView,
 )
 
-# from user.views import SpartaTokenObtainPairView
-# from user.views import OnlyAuthenticatedUserView
-
 urlpatterns = [
     path('', views.UserView.as_view()),
-    path('login/', views.UserAPIView.as_view()),
-    path('logout/', views.UserAPIView.as_view()),
-    path('<obj_id>/', views.UserView.as_view()),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', views.TokenObtainPairView.as_view(), name='sparta_token'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/authonly/', views.OnlyAuthenticatedUserView.as_view()),
+    path('<obj_id>/', views.OnlyAuthenticatedUserView.as_view()),
 ]
