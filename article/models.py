c11a4bb03230c7b768580ed9fc6ed372bd81bc5c
joohuun <k00005781@gmail.com>
2022-06-30 21:31:40 +0900

    Edit RatingModel

--- article/models.py
+++ article/models.py
@@ -29,7 +29,6 @@ class Article(BaseModel):
 
 
 class Comment(models.Model):
-    
     article = models.ForeignKey(Article, verbose_name="원글", on_delete=models.CASCADE)
     user = models.ForeignKey('user.User', verbose_name="작성자", on_delete=models.CASCADE)
     comment = models.TextField("댓글 내용")
@@ -41,5 +40,8 @@ class Comment(models.Model):
 class Rating(models.Model):
     user = models.OneToOneField('user.User', on_delete=models.SET_NULL, null=True)
     article = models.OneToOneField(Article, on_delete=models.SET_NULL, null=True)
-    rating = models.IntegerChoices('평점','1 2 3 4 5')
+    rating = models.IntegerField("평점", null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.user.username}/{self.rating}점"
     
