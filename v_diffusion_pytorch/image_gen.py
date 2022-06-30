09f72510e87cdd4f8dda45b4960c7b8c3384a181
SG-Marco <rollingpoe@gmail.com>
2022-06-30 22:38:20 +0900

    feat - 생성된 이미지 서버에 저장. 사용자가 게시글 생성 안할시에 삭제

--- v_diffusion_pytorch/image_gen.py
+++ v_diffusion_pytorch/image_gen.py
@@ -1,53 +1,56 @@
-
 import gc
 import math
 import sys
-
 import torch
 from torchvision import utils as tv_utils
-
 from tqdm.notebook import tqdm
+import random
+from PIL import ImageFile, Image
+import numpy as np
+import os
+from datetime import datetime
 
 sys.path.append('/v-diffusion-pytorch')
 from .CLIP import clip
 from .diffusion import get_model, sampling, utils
 
+# device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+'''
+Gpu 사용 시에는 'cpu' 부분을 'cuda'로
+    .cpu() 는 .cuda()로 변경
+model 부분만 half()가 추가되어서 주석으로 남겨놓음
+'''
+
 model = get_model('cc12m_1_cfg')()
 _, side_y, side_x = model.shape
 model.load_state_dict(torch.load('cc12m_1_cfg.pth', map_location='cpu'))
-model = model.half().cuda().eval().requires_grad_(False)
+
+# model = model.half().cuda().eval().requires_grad_(False)
+model = model.cpu().eval().requires_grad_(False)
+
 clip_model = clip.load(model.clip_model, jit=False, device='cpu')[0]
 
 
-import random
- 
+##################################################
 
 height =  256
 width =  256
 side_x = width
 side_y = height
-
-steps =   25
-
-n_images =   4
-
-weight = 3
-
-eta =   0
-
+steps = 1
+n_images = 4
+weight = 3 # 3이 디폴트
+eta = 0
 display_every = 5  
-
 save_progress_video = True 
-
 save_name = 0.00000000
 
-from PIL import ImageFile, Image
-import numpy as np
-import os
+##################################################################
 
-def run(prompt):
-    target_embed = clip_model.encode_text(clip.tokenize(prompt)).float().cuda()
-
+def run(username, prompt):
+    target_embed = clip_model.encode_text(clip.tokenize(prompt)).float().cpu()
+    now = datetime.now().strftime('%Y%m%d%H%M%S')
+    
     def cfg_model_fn(x, t):
         """The CFG wrapper function."""
         n = x.shape[0]
@@ -74,25 +77,26 @@ def run(prompt):
             tqdm.write(f'')
 
     print("Prompt is: " + prompt)
-    print("hello"+prompt)
     
     seed = random.randint(0, 2**32)
     print("Seed is: " + str(seed))
     gc.collect()
     torch.cuda.empty_cache()
     torch.manual_seed(seed)
-    x = torch.randn([n_images, 3, side_y, side_x], device='cuda')
-    t = torch.linspace(1, 0, steps + 1, device='cuda')[:-1]
+    x = torch.randn([n_images, 3, side_y, side_x], device='cpu')
+    t = torch.linspace(1, 0, steps + 1, device='cpu')[:-1]
     step_list = utils.get_spliced_ddpm_cosine_schedule(t)
     outs = sampling.sample(cfg_model_fn, x, step_list, eta, {}, callback=display_callback)
     tqdm.write('Done!')
+    
+    
     for i, out in enumerate(outs):
-        filename = f'static/images/out_{i}.png'
+        filename = f'static/images/{username}_{now}_{i}.png'
         utils.to_pil_image(out).save(filename)
     
     frames = []
     files = []
-    init_frame = 1 
+    init_frame = 0
     last_frame = steps 
 
     directory = 'static/images/steps'
@@ -101,6 +105,11 @@ def run(prompt):
         files.append(f)
     for i in range(init_frame,last_frame): 
         frames.append(Image.open(files[i]))
-    frames[-1].save("static/images/finalgrid.png")
+    frames[-1].save(f"static/images/{username}_{now}_finalgrid.png")
 
+    # steps에 저장된 이미지들은 바로 삭제
+    for filename in os.listdir(directory):
+        os.remove(f'{directory}/{filename}')  
+
+    return f"{username}_{now}"
 
